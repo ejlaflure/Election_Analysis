0c507ba6f17768e41ffaac0caa262a80d5fbe60a
Eric LaFlure <ejlaflure@gmail.com>
2020-04-17 13:17:40 -0500

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -1,28 +1,82 @@
 #Open data file.
-import csv
 import os
+import csv
+
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("Analysis", "election_analysis.txt")
 
+# Initialize a total vote counter.
+total_votes = 0
+
+# Candidate Options and Candidate Votes
+candidate_options = []
+# Declare the empty dictionary
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
-    # Print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+  
+     # Read each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+        
+        # Find the candidate names from each row.
+        candidate_name = row[2]
 
-#The data we need to retrieve
-#1. Total number of votes cast
-#2. A complete list of candidates who received votes
-#3. Total number of votes each candidate received
-#4. Percentage of votes each candidate won
-#5. The winner of the election based on popular vote
+        if candidate_name not in candidate_options:
 
-# Using the with statement open the file as a text file.
-#with open(file_to_save, "w") as txt_file:
+           # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
 
-    # Write the rerived data to the file.
-     #txt_file.write("")
+           # Begin tracking that candidate's vote count. 
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+# Write data we need to retrieve to our text file using a with statement.
+with open(file_to_save, "w") as txt_file:
+    #Total number of votes cast
+    txt_file.write(f"Total number of votes cast: {total_votes}\n")
+
+    # Determine the percentage of votes for each candidate by looping through the counts.
+    # Iterate through the candidate list.
+    for candidate in candidate_votes:
+        # Retrieve vote count of a candidate.
+        votes = candidate_votes[candidate]
+        # Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+        #Iterate through the candidate list.
+        txt_file.write(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+        
+        # Determine winning vote count and candidate
+        # Determine if the votes is greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+         # If true then set winning_count = votes and winning_percent =
+         # vote_percentage.
+         winning_count = votes
+         winning_percentage = vote_percentage
+         # And, set the winning_candidate equal to the candidate's name.
+         winning_candidate = candidate
+    
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+
+    #The winner of the election based on popular vote
+    txt_file.write(winning_candidate_summary)
